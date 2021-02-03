02966dcc6ae9face4c83f3112fe6260a31d89e09
8ru73u5 <brute.us@protonmail.com>
2021-02-03 19:36:00 +0100

    Add AppOption type

--- cws/models.py
+++ cws/models.py
@@ -1,7 +1,12 @@
 from __future__ import annotations
 
+from enum import Enum
+from typing import Optional, Any
+
 from sqlalchemy import Column, Integer, String, Boolean, ForeignKey, ForeignKeyConstraint
-from sqlalchemy.orm import relationship
+from sqlalchemy.dialects.postgresql import JSONB
+from sqlalchemy.exc import SQLAlchemyError
+from sqlalchemy.orm import relationship, Session
 
 from .database import Base
 
@@ -90,3 +95,82 @@ class Bet(Base):
 
     def __hash__(self):
         return hash((self.id, self.market_id, self.sport_id))
+
+
+class AppOption(Base):
+    class OptionType(Enum):
+        MIN_ODDS = {
+            'id': 1,
+            'default': 1.0,
+            'check': lambda v: float(v) >= 1.0,
+            'name': 'Minimum odds'
+        }
+        MAX_ODDS = {
+            'id': 2,
+            'default': 15.0,
+            'check': lambda v: float(v) >= 1.1,
+            'name': 'Maximum odds'
+        }
+        TELEGRAM_NOTIFICATION_MIN_UPTIME = {
+            'id': 3,
+            'default': 120,
+            'check': lambda v: int(v) >= 5,
+            'name': 'Telegram notification after × seconds'
+        }
+        SOUND_NOTIFICATION_MIN_UPTIME = {
+            'id': 4,
+            'default': 60,
+            'check': lambda v: int(v) >= 5,
+            'name': 'Sound notification after × seconds'
+        }
+
+        @staticmethod
+        def get_by_id(option_id: int) -> Optional[dict]:
+            return next((o.value for o in AppOption.OptionType.__members__.values() if o.value['id'] == option_id), None)
+
+    __tablename__ = 'options'
+
+    id = Column(Integer, primary_key=True, autoincrement=False)
+    value = Column(JSONB, nullable=False)
+
+    # noinspection PyBroadException
+    def check(self) -> bool:
+        option = AppOption.OptionType.get_by_id(self.id)
+
+        try:
+            return option['check'](self.value['value'])
+        except:
+            return False
+
+    @staticmethod
+    def get_option(option_type: AppOption.OptionType, session: Session) -> Optional[Any]:
+        o = session.query(AppOption).get(option_type.value['id'])
+
+        if o is not None:
+            return o.value['value']
+        else:
+            return None
+
+    @classmethod
+    def insert_default_options(cls, session: Session):
+        db_error = None
+
+        try:
+            for option in cls.OptionType.__members__.values():
+                o = AppOption(id=option.value['id'], value={
+                    'name': option.value['name'],
+                    'value': option.value['default']
+                })
+
+                if o.check():
+                    session.add(o)
+
+            session.commit()
+        except SQLAlchemyError as e:
+            db_error = e
+            session.rollback()
+        finally:
+            session.close()
+
+        if db_error is not None:
+            raise db_error
